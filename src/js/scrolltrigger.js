26cc8eee0d6c5cb42c6bafc085ea124aad6fe1da
Roman Isaiev <107892291+RomanIsaiev@users.noreply.github.com>
2024-07-24 20:10:47 +0300

    website complete

---
+++ src/js/scrolltrigger.js
@@ -0,0 +1,42 @@
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+// gsap.registerPlugin(ScrollTrigger);
+
+// // gsap.utils.toArray('.panel').forEach((panel, i) => {
+// //   ScrollTrigger.create({
+// //     trigger: panel,
+// //     start: 'top top',
+// //     pin: true,
+// //     pinSpacing: false,
+// //   });
+// // });
+
+// gsap.utils.toArray('.panel').forEach((panel, i) => {
+//   ScrollTrigger.create({
+//     trigger: panel,
+//     start: 'top top+=100',
+//     pin: true,
+//     pinSpacing: false,
+//     scrub: true,
+//     end: '+=500',
+//   });
+// });
+
+// ScrollTrigger.create({
+//   snap: false, // snap whole page to the closest section!
+// });
+
+// gsap.registerPlugin(ScrollTrigger);
+
+// let panels = gsap.utils.toArray('.panel');
+
+// panels.forEach((panel, i) => {
+//   ScrollTrigger.create({
+//     trigger: panel,
+//     start: 'top top',
+//     pin: i === panels.length - 1 ? false : true,
+//     end: 'bottom 100',
+//     pinSpacing: false,
+//   });
+// });
